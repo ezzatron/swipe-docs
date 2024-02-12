97702a254dbfc7f21be62e9b2d2a70a922db4f5c
Erin Millard <contact@erin.id.au>
2024-02-12 23:52:04 +1000

    Integrate rehype-pretty-code

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,19 @@
 import createMDX from "@next/mdx";
+import rehypePrettyCode from "rehype-pretty-code";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
 };
 
+/** @type {import('rehype-pretty-code').Options} */
+const rehypePrettyCodeOptions = {
+  defaultLang: "plaintext",
+};
+
 const withMDX = createMDX({
   options: {
-    rehypePlugins: [],
+    rehypePlugins: [[rehypePrettyCode, rehypePrettyCodeOptions]],
   },
 });
 
--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -2,6 +2,7 @@ import type { MDXComponents } from "mdx/types";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    wrapper: ({ children }) => <div className="mdx-wrapper">{children}</div>,
     ...components,
   };
 }
