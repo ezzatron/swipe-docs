6e8b62bb1a30f9391f9a28db0d91e7fffa9cba75
Erin Millard <contact@erin.id.au>
2024-12-24 09:52:19 +1000

    Revert to standard dist dir for Vercel

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import createMDX from "@next/mdx";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  distDir: "artifacts/next/dist",
   pageExtensions: ["js", "jsx", "ts", "tsx", "mdx"],
   experimental: {
     mdxRs: true,
