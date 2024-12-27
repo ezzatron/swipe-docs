2e249aa3d8086c057e9375409183e48dba9797b1
Erin Millard <contact@erin.id.au>
2024-12-27 16:37:26 +1000

    Add code block titles

--- src/app/components/code.tsx
+++ src/app/components/code.tsx
@@ -5,10 +5,14 @@ export async function Code({ codeblock }: { codeblock: RawCode }) {
   const highlighted = await highlight(codeblock, "github-dark");
 
   return (
-    <Pre
-      code={highlighted}
-      handlers={[callout]}
-      className="border border-zinc-800"
-    />
+    <div className="border border-zinc-700 mb-6 rounded">
+      {highlighted.meta && (
+        <div className="px-3 py-2 text-sm border-b border-zinc-700 bg-zinc-900 rounded-t">
+          {highlighted.meta}
+        </div>
+      )}
+
+      <Pre code={highlighted} handlers={[callout]} className="mt-0 mb-0" />
+    </div>
   );
 }
