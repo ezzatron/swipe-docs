996cd785037eb42c54e881f8a3914412a9fa89dc
Erin Millard <contact@erin.id.au>
2024-12-30 13:32:45 +1000

    Make line numbers optional

--- src/code-block/Code.tsx
+++ src/code-block/Code.tsx
@@ -1,24 +1,55 @@
-import { Pre, RawCode, highlight } from "codehike/code";
+import { Pre, RawCode, highlight, type AnnotationHandler } from "codehike/code";
 import { callout } from "./annotations/callout";
 import { lineNumbers } from "./annotations/line-numbers";
 import CopyButton from "./CopyButton";
 
-export default async function Code({ codeblock }: { codeblock: RawCode }) {
+const FLAG_PATTERN = /^-(.*)$/;
+
+type Props = {
+  codeblock: RawCode;
+};
+
+export default async function Code({ codeblock }: Props) {
   const highlighted = await highlight(codeblock, "github-dark");
+  const { code, meta } = highlighted;
+  const [title, flags] = parseMeta(meta);
+
+  const handlers: AnnotationHandler[] = [
+    ...(flags.lineNumbers ? [lineNumbers] : []),
+    callout,
+  ];
 
   return (
     <div className="mb-6 rounded border border-zinc-700">
       <div className="flex justify-between rounded-t border-b border-zinc-700 bg-zinc-900 px-3 py-2 text-sm">
-        <div>{highlighted.meta}</div>
-
-        <CopyButton text={highlighted.code} />
+        <div>{title}</div>
+        <CopyButton text={code} />
       </div>
 
-      <Pre
-        code={highlighted}
-        handlers={[callout, lineNumbers]}
-        className="mb-0 mt-0"
-      />
+      <Pre code={highlighted} handlers={handlers} className="mb-0 mt-0" />
     </div>
   );
+}
+
+type Flags = {
+  lineNumbers: boolean;
+};
+
+function parseMeta(meta: string): [title: string, flags: Flags] {
+  let title = "";
+  const flags: Flags = {
+    lineNumbers: false,
+  };
+
+  for (const part of meta.split(" ")) {
+    const match = FLAG_PATTERN.exec(part);
+
+    if (match) {
+      if (match[1] === "n") flags.lineNumbers = true;
+    } else {
+      title = part;
+    }
+  }
+
+  return [title, flags];
 }
