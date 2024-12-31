b53969101152ae46ddf1a941c193f962358ea102
Erin Millard <contact@erin.id.au>
2024-12-31 15:07:21 +1000

    Improve typing for CodeBlock component lang prop

--- src/code-block/CodeBlock.tsx
+++ src/code-block/CodeBlock.tsx
@@ -1,11 +1,12 @@
+import { type LighterResult } from "@code-hike/lighter";
 import Code from "./Code";
 import { buildMeta, type Meta } from "./meta";
 
 type Props = {
   children: string;
-  lang?: string;
+  lang?: LighterResult["lang"];
 } & Partial<Meta>;
 
-export default function CodeBlock({ children, lang = "text", ...meta }: Props) {
+export default function CodeBlock({ children, lang = "txt", ...meta }: Props) {
   return <Code codeblock={{ value: children, lang, meta: buildMeta(meta) }} />;
 }
