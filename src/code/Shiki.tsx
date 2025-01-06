b7ccf6b6bf365a84497af9a1d37aa31e71f57a62
Erin Millard <contact@erin.id.au>
2025-01-06 12:57:42 +1000

    Support different start line numbers

--- src/code/Shiki.tsx
+++ src/code/Shiki.tsx
@@ -10,11 +10,26 @@ type Props = {
 };
 
 export default function Shiki({ tree }: Props) {
+  const startLine = 123;
+  const lineCount = countLines(tree);
+  const endLine = startLine + lineCount - 1;
+  const lineNumberWidth = `${endLine.toString().length}ch`;
+
   return toJsxRuntime(tree, {
     Fragment,
     jsx,
     jsxs,
     components: {
+      code: (props) => (
+        <code
+          {...props}
+          style={{
+            "--line-number-width": lineNumberWidth,
+            "--start-line": startLine,
+          }}
+        />
+      ),
+
       pre: ({
         className,
         ...props
@@ -30,4 +45,28 @@ export default function Shiki({ tree }: Props) {
       ),
     },
   });
+}
+
+function countLines(root: Root): number {
+  const [pre] = root.children;
+
+  if (pre?.type !== "element" || pre?.tagName !== "pre") {
+    throw new Error("Unexpected Shiki AST content");
+  }
+
+  const [code] = pre.children;
+
+  if (code?.type !== "element" || code?.tagName !== "code") {
+    throw new Error("Unexpected Shiki AST content");
+  }
+
+  let lineCount = 0;
+
+  for (const child of code.children) {
+    if (child.type === "element" && child.properties.class === "line") {
+      ++lineCount;
+    }
+  }
+
+  return lineCount;
 }
