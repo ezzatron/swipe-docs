5f4ae70efed49d161c602989d2741fd7350edb2d
Erin Millard <contact@erin.id.au>
2025-01-08 10:34:30 +1000

    Replace Makefile example with Go

---
+++ src/example/lorem-ipsum.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+const Lorem = 3.14
+
+type Ipsum struct {
+	Dolor string
+	Sit   int
+}
+
+func (i Ipsum) Amet() string {
+	return fmt.Sprintf("Hello, my name is %s and I am %d years old.", i.Dolor, i.Sit)
+}
+
+func lorem(a, b int) int {
+	return a + b
+}
+
+func ipsum(a, b float64) (float64, error) {
+	if b == 0 {
+		return 0, fmt.Errorf("cannot divide by zero")
+	}
+	return a / b, nil
+}
--- src/example/lorem-ipsum.mk
+++
@@ -1,8 +0,0 @@
-.PHONY: lorem
-lorem: ipsum
-	@echo "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-
-.PHONY: ipsum
-ipsum:
-	@echo "Sed do eiusmod tempor incididunt ut" \
-		"labore et dolore magna aliqua."
