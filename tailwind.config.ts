c3e90b5cc58a30248bf7ff0b0ccf640e769a690c
Erin Millard <contact@erin.id.au>
2025-01-08 10:58:04 +1000

    Improve inline code styling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
 import typography from "@tailwindcss/typography";
 import type { Config } from "tailwindcss";
+import type { PluginUtils } from "tailwindcss/types/config";
 
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
@@ -11,10 +12,16 @@ export default {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      typography: {
+      typography: ({ theme }: PluginUtils) => ({
         DEFAULT: {
           css: {
             code: {
+              backgroundColor: theme("colors.gray.800"),
+              borderRadius: theme("borderRadius.DEFAULT"),
+              color: null,
+              fontWeight: null,
+              paddingBlock: theme("padding.1"),
+              paddingInline: theme("padding.2"),
               tabSize: "2",
             },
             "code::before": {
@@ -25,7 +32,7 @@ export default {
             },
           },
         },
-      },
+      }),
     },
   },
 } satisfies Config;
