9e1e4b78488537c20335e3eaaabdc48e623cb1fe
Erin Millard <contact@erin.id.au>
2025-01-09 18:57:58 +1000

    Expand language icons

--- src/code/LanguageIcon.tsx
+++ src/code/LanguageIcon.tsx
@@ -1,4 +1,8 @@
-import { JavascriptPlain, TypescriptPlain } from "devicons-react";
+import {
+  JavascriptPlain,
+  MarkdownOriginal,
+  TypescriptPlain,
+} from "devicons-react";
 import { FileTextIcon, SquareTerminalIcon } from "lucide-react";
 
 type Props = {
@@ -8,12 +12,17 @@ type Props = {
 export default function LanguageIcon({ lang }: Props) {
   switch (lang) {
     case "javascript":
+    case "jsx":
       return <JavascriptPlain size={16} color="currentcolor" />;
-    case "typescript":
-      return <TypescriptPlain size={16} color="currentcolor" />;
+    case "markdown":
+    case "mdx":
+      return <MarkdownOriginal size={16} fill="currentcolor" />;
     case "shellscript":
     case "shellsession":
       return <SquareTerminalIcon size={16} />;
+    case "tsx":
+    case "typescript":
+      return <TypescriptPlain size={16} color="currentcolor" />;
   }
 
   return <FileTextIcon size={16} />;
