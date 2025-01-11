8df720d91503942c22ced41f6b35aaad321db250
Erin Millard <contact@erin.id.au>
2025-01-11 20:53:45 +1000

    Remove devicons

--- src/code/LanguageIcon.tsx
+++ src/code/LanguageIcon.tsx
@@ -1,8 +1,3 @@
-import {
-  JavascriptPlain,
-  MarkdownOriginal,
-  TypescriptPlain,
-} from "devicons-react";
 import { FileTextIcon, SquareTerminalIcon } from "lucide-react";
 
 type Props = {
@@ -11,18 +6,9 @@ type Props = {
 
 export default function LanguageIcon({ lang }: Props) {
   switch (lang) {
-    case "javascript":
-    case "jsx":
-      return <JavascriptPlain size={16} color="currentcolor" />;
-    case "markdown":
-    case "mdx":
-      return <MarkdownOriginal size={16} fill="currentcolor" />;
     case "shellscript":
     case "shellsession":
       return <SquareTerminalIcon size={16} />;
-    case "tsx":
-    case "typescript":
-      return <TypescriptPlain size={16} color="currentcolor" />;
   }
 
   return <FileTextIcon size={16} />;
