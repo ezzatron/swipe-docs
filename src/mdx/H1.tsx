2c6d6cb1354060eae33bec9fb2606d59276fa136
Erin Millard <contact@erin.id.au>
2025-02-05 08:53:31 +1000

    Make MDX H1 honor existing props

--- src/mdx/H1.tsx
+++ src/mdx/H1.tsx
@@ -1,7 +1,13 @@
+import clsx from "clsx";
 import type { JSX } from "react";
 
 type Props = JSX.IntrinsicElements["h1"];
 
-export default function H1({ children }: Props) {
-  return <h1 className="text-4xl font-extrabold tracking-tight">{children}</h1>;
+export default function H1({ className, ...props }: Props) {
+  return (
+    <h1
+      {...props}
+      className={clsx(className, "text-4xl font-extrabold tracking-tight")}
+    />
+  );
 }
