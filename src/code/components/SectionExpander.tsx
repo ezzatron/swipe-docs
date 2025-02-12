1fd036d941938d31ab8d5c27eb136bfce7295670
Erin Millard <contact@erin.id.au>
2025-02-12 10:43:02 +1000

    Improve section styles

--- src/code/components/SectionExpander.tsx
+++ src/code/components/SectionExpander.tsx
@@ -2,7 +2,7 @@ import { ChevronDownIcon, ChevronUpIcon } from "lucide-react";
 
 export default function SectionExpander() {
   return (
-    <label className="cb-se group has-focus-visible:focus-outline relative flex justify-center rounded-b-sm bg-gray-200 py-0.5 text-sm text-gray-600 has-focus-visible:-outline-offset-2 sm:items-start dark:bg-gray-800 dark:text-gray-400">
+    <label className="cb-se group has-focus-visible:focus-outline relative flex cursor-pointer justify-center rounded-b-sm border-t-1 border-dashed border-[var(--cb-context-border)] bg-[var(--cb-bg)] py-1 text-sm has-focus-visible:-outline-offset-2 sm:items-start">
       <input
         type="checkbox"
         className="absolute size-0 appearance-none focus-visible:outline-0"
