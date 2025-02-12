4f7d150576a0a3e2493ec2f51c90017529ef41f7
Erin Millard <contact@erin.id.au>
2025-02-12 10:50:05 +1000

    Add titles to expanders

--- src/code/components/SectionExpander.tsx
+++ src/code/components/SectionExpander.tsx
@@ -2,19 +2,27 @@ import { ChevronDownIcon, ChevronUpIcon } from "lucide-react";
 
 export default function SectionExpander() {
   return (
-    <label className="cb-se group has-focus-visible:focus-outline relative flex cursor-pointer justify-center rounded-b-sm border-t-1 border-dashed border-[var(--cb-context-border)] bg-[var(--cb-bg)] py-1 text-sm has-focus-visible:-outline-offset-2 sm:items-start">
+    <label className="cb-se group has-focus-visible:focus-outline relative block cursor-pointer justify-center rounded-b-sm border-t-1 border-dashed border-[var(--cb-context-border)] bg-[var(--cb-bg)] py-1 text-sm has-focus-visible:-outline-offset-2">
       <input
         type="checkbox"
         className="absolute size-0 appearance-none focus-visible:outline-0"
       />
 
-      <span aria-label="Show more" className="group-has-checked:hidden">
+      <div
+        aria-label="Show more"
+        title="Show more"
+        className="flex justify-center group-has-checked:hidden"
+      >
         <ChevronDownIcon size={16} aria-hidden />
-      </span>
+      </div>
 
-      <span aria-label="Show less" className="hidden group-has-checked:block">
+      <div
+        aria-label="Show less"
+        title="Show less"
+        className="hidden justify-center group-has-checked:flex"
+      >
         <ChevronUpIcon size={16} aria-hidden />
-      </span>
+      </div>
     </label>
   );
 }
