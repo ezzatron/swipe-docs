484d1c4e955f5f23ebb8bf03efe6a8e817e50cc4
Erin Millard <contact@erin.id.au>
2025-07-27 20:09:35 +1000

    WIP

--- src/composable/CodeBlockExpandButton.tsx
+++ src/composable/CodeBlockExpandButton.tsx
@@ -36,7 +36,7 @@ export default function CodeBlockExpandButton({
         <Dialog.Overlay className="fixed inset-0 bg-black/50" />
         <Dialog.Content
           aria-describedby=""
-          className="fixed top-1/2 left-1/2 max-h-[85vh] -translate-x-1/2 -translate-y-1/2 shadow-[var(--shadow-6)] focus:outline-none"
+          className="fixed top-1/2 left-1/2 max-h-[85vh] max-w-[calc(100%---spacing(16))] -translate-x-1/2 -translate-y-1/2 shadow-[var(--shadow-6)] focus:outline-none"
         >
           <CodeBlockRoot>
             <CodeBlockFrame>
